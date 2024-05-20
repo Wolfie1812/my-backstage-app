c50ea7c5c6eb44dcd166348a565f121b6a20356f
Ileen de Wolf <ileen.de.wolf@nn.nl>
2024-05-20 13:26:35 +0200

    configure basics

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -1,6 +1,43 @@
+import {
+  HomePageStarredEntities,
+  WelcomeTitle,
+  CustomHomepageGrid,
+} from '@backstage/plugin-home';
+import { HomePageSearchBar } from '@backstage/plugin-search';
 import React from 'react';
 
+// This is the default configuration that is shown to the user
+// when first arriving to the homepage.
+const defaultConfig = [
+  {
+    component: 'HomePageSearchBar',
+    x: 0,
+    y: 0,
+    width: 12,
+    height: 5,
+  },
+  {
+    component: 'HomePageStarredEntities',
+    x: 0,
+    y: 2,
+    width: 6,
+    height: 12,
+  },
+  {
+    component: 'WelcomeTitle',
+    x: 6,
+    y: 2,
+    width: 6,
+    height: 16,
+  },
+];
+
 export const HomePage = () => (
-  /* We will shortly compose a pretty homepage here. */
-  <h1>Welcome to Backstage!</h1>
+  <CustomHomepageGrid config={defaultConfig} rowHeight={10}>
+    // Insert the allowed widgets inside the grid. User can add, organize and //
+    remove the widgets as they want.
+    <HomePageSearchBar />
+    <WelcomeTitle />
+    <HomePageStarredEntities />
+  </CustomHomepageGrid>
 );
---
+++ plugins/notifications-api-backend/src/index.ts
@@ -0,0 +1,2 @@
+export * from './service/router';
+export { notificationsApiPlugin as default } from './plugin';
